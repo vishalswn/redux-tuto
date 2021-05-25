07b028a30c5fdcb6339264789dbcc4d4b9794453
vishal <vishal.thoughtwin@gmail.com>
2021-05-25 14:14:12 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
+import HomeContainer from './containers/HomeContainers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <h1>App Component</h1> */}
+     <User data={{name: "vishal sharma",age:26}} />
+     <HomeContainer/> 
     </div>
   );
 }
